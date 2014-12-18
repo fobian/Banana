e41b8d731ee574f3db4c77c4d5812afc87021f3f
macainiao <chijiabao@gmail.com>
2014-12-18 16:06:50 +0800

    Delete unused code

--- app/src/main/java/com/chijsh/banana/ui/LoginActivity.java
+++ app/src/main/java/com/chijsh/banana/ui/LoginActivity.java
@@ -3,22 +3,16 @@ package com.chijsh.banana.ui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.chijsh.banana.AccessTokenKeeper;
-import com.chijsh.banana.Config;
 import com.chijsh.banana.R;
 import com.chijsh.banana.presenter.LoginPresenter;
 import com.chijsh.banana.presenter.LoginPresenterImpl;
 import com.chijsh.banana.presenter.LoginView;
 import com.chijsh.banana.utils.Utility;
 import com.sina.weibo.sdk.auth.Oauth2AccessToken;
-import com.sina.weibo.sdk.auth.WeiboAuth;
-import com.sina.weibo.sdk.auth.WeiboAuthListener;
-import com.sina.weibo.sdk.auth.sso.SsoHandler;
 import com.sina.weibo.sdk.exception.WeiboException;
 
 import butterknife.ButterKnife;
@@ -30,7 +24,6 @@ public class LoginActivity extends BaseActivity implements LoginView {
     @InjectView(R.id.login)
     TextView mLoginButton;
 
-    private Oauth2AccessToken mAccessToken;
     private LoginPresenter mPresenter;
 
     @Override
@@ -51,27 +44,20 @@ public class LoginActivity extends BaseActivity implements LoginView {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        // SSO 授权回调
-        // 重要：发起 SSO 登陆的 Activity 必须重写 onActivityResult
+
         mPresenter.onActivityResult(requestCode, resultCode, data);
 
     }
 
     @Override
     public void loginCompleted(Bundle bundle) {
-        mAccessToken = Oauth2AccessToken.parseAccessToken(bundle);
-        if (mAccessToken.isSessionValid()) {
-
-            // 保存 Token 到 SharedPreferences
-            AccessTokenKeeper.writeAccessToken(this, mAccessToken);
+        Oauth2AccessToken accessToken = Oauth2AccessToken.parseAccessToken(bundle);
+        if (accessToken.isSessionValid()) {
+            AccessTokenKeeper.writeAccessToken(this, accessToken);
             Toast.makeText(this, R.string.weibo_auth_success, Toast.LENGTH_SHORT).show();
             startActivity(new Intent(this, TimeLineActivity.class));
             finish();
         } else {
-            // 以下几种情况，您会收到 Code：
-            // 1. 当您未在平台上注册的应用程序的包名与签名时；
-            // 2. 当您注册的应用程序包名与签名不正确时；
-            // 3. 当您在平台上注册的包名和签名与您当前测试的应用的包名和签名不匹配时。
             String code = bundle.getString("code");
             String message = getString(R.string.weibo_auth_failed);
             if (!TextUtils.isEmpty(code)) {
--- app/src/main/java/com/chijsh/banana/utils/Utility.java
+++ app/src/main/java/com/chijsh/banana/utils/Utility.java
@@ -1,20 +1,10 @@
 package com.chijsh.banana.utils;
 
 import android.content.Context;
-import android.graphics.Bitmap;
 import android.os.Build;
-import android.renderscript.Allocation;
-import android.renderscript.Element;
-import android.renderscript.RenderScript;
-import android.renderscript.ScriptIntrinsicBlur;
 import android.widget.Toast;
 
 import com.chijsh.banana.app.MyApplication;
-
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.TimeZone;
 
 /**
  * Created by chijsh on 10/28/14.
